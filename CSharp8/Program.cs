5798d58778c6e2b8ca334504509a1a9d0970d847
vludlss <vladislav.mikkoev@yandex.ru>
2024-01-16 05:02:47 +0300

    feat: Члены экземпляров readonly C# 8

---
+++ CSharp8/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CSharp8/TestReadonlyMember.cs
@@ -0,0 +1,52 @@
+﻿namespace CSharp8
+{
+    /// <summary>
+    /// Компилятор может использовать модификатор readonly для оптимизации производительности.
+    /// </summary>
+    internal struct TestReadonlyMember
+    {
+        private int Number1 { get; set; }
+        private int Number2 { get; set; }
+        private readonly int Number3 { get; init; }
+
+        public TestReadonlyMember(int number1, int number2)
+        {
+            Number1 = number1;
+            Number2 = number2;
+        }
+
+        /// <remarks>
+        /// Присвоить данные Number1 и Number2 в этом методе не получиться
+        /// </remarks>
+        public readonly string GetString()
+        {
+            //Number1 = 2;
+            return $"{Number1} {Number2}";
+        }
+
+        /// <remarks>
+        /// Здесь тоже
+        /// </remarks>
+        public readonly override string ToString()
+        {
+            //Number1 = 2;
+            return $"{Number1} {Number2}";
+        }
+
+        /// <remarks>
+        /// Здесь тоже
+        /// </remarks>
+        public int Sum
+        {
+            readonly get
+            {
+                //Number1 = 2;
+                return Number1 + Number2;
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
