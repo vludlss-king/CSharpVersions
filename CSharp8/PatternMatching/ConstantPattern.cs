35a72b64f2c3f6f77e026e0cb2fb1f835a91f392
vludlss <vladislav.mikkoev@yandex.ru>
2024-01-16 05:56:27 +0300

    feat: ConstantPattern C# 8

---
+++ CSharp8/PatternMatching/ConstantPattern.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp8.PatternMatching
+{
+    internal class ConstantPattern
+    {
+        public ConstantPattern()
+        {
+            var input = "";
+
+            // Эквивалент input == null
+            if(input is null)
+            {
+
+            }
+        }
+
+        /// <remarks>
+        /// Можно использовать
+        /// int, float, char, string, bool, enum, const field, null
+        /// </remarks>
+        public decimal GetGroupTicketPrice(int visitorCount) => visitorCount switch
+        {
+            1 => 12.0m,
+            2 => 20.0m,
+            3 => 27.0m,
+            4 => 32.0m,
+            0 => 0.0m,
+            _ => throw new ArgumentException()
+        };
+    }
+}
